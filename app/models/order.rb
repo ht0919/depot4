b18a51f616e0d3975682de8592f38cb7f0d0560b
Hiroyuki Tanaka <ht0919@gmail.com>
2017-07-28 14:26:43 +0900

    Japaneseization of menu

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,8 @@
 class Order < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
   # ...
-  PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
+  #PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
+  PAYMENT_TYPES = [ "現金", "クレジットカード", "注文書" ]
   # ...
   validates :name, :address, :email, presence: true
   validates :pay_type, inclusion: PAYMENT_TYPES
